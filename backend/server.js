409377e5d80b45337096b08c96b62fd816fa5235
Aniket <aniket.shrirao@publicissapient.com>
2023-06-14 17:59:08 +0530

    feat-initiated the setup for my fantasyAnalysis website

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect('mongodb://localhost:27017/fantasyAnalysis', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
